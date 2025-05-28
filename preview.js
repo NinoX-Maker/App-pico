865f376871508de06ca45c2b8dabc80e91879edd
NinoX-Maker <agta2008.avila@gmail.com>
2025-05-27 21:51:46 -0300

    Adiciona preview.js

--- preview.js
+++ preview.js
@@ -1,13 +1,13 @@
 const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('load-audio', (event, audioDataUrl) => {
-<<<<<<< HEAD
+
   const player = document.getElementById('player');
   player.src = audioDataUrl;
 });
-=======
+
     console.log('audio recebido na pre-visualização', audioDataUrl)
     const player = document.getElementById('player');
     player.src = audioDataUrl;
+
 });
->>>>>>> abb30294ebb89c47b9cd89cf311257921ea5444b
